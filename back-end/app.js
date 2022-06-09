97e733d19514849fc69a8af9c1da657997430e9b
jmahirakz <jmahirakz@paypal.com>
2022-06-09 22:55:35 +0530

    __dirname in 'module'

--- back-end/app.js
+++ back-end/app.js
@@ -1,9 +1,10 @@
-// import express from 'express';
-// import cors from 'cors';
+import express from 'express';
+import cors from 'cors';
+import { resolve } from 'path';
 
-const express = require('express')
-const cors = require('cors')
-const { resolve } = require('path')
+// const express = require('express')
+// const cors = require('cors')
+// const { resolve } = require('path')
 
 const PORT = process.env.PORT || 4000
 const app = express();
@@ -13,6 +14,12 @@ const corsOptions = {
   optionsSuccessStatus: 200
 }
 // const buildFolderPath = '/Users/jmahirakz/Sample-Deployment/front-end/build'
+//Do the following only for 'module'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __fileName = fileURLToPath(import.meta.url)
+const __dirname = dirname(__fileName)
 const buildFolderPath = resolve(__dirname, '../front-end/build')
 
 app.use(cors(corsOptions))
