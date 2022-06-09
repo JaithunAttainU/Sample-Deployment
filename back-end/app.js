194b0e24f7e266fa67ad8770a8b2f4817103a296
jmahirakz <jmahirakz@paypal.com>
2022-06-09 22:15:30 +0530

    FE Build

--- back-end/app.js
+++ back-end/app.js
@@ -9,11 +9,20 @@ const corsOptions = {
   origin: "http://localhost:3000",
   optionsSuccessStatus: 200
 }
+const buildFolderPath = '/Users/jmahirakz/Sample-Deployment/front-end/build'
 
 app.use(cors(corsOptions))
+app.use(express.static(buildFolderPath))
 app.get('/api/products', (req, res) => {
   res.json(allProducts)
 });
+
+
+
+app.get('*', (req, res) => {
+  res.sendFile(`${buildFolderPath}/index.html`)
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
